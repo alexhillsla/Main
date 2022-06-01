fed2451d22718a54936cf664b8efed91b9adcfe7
alexhills.la@gmail.com <aleks@Aleksandrs-MacBook-Pro.local>
2022-06-01 11:33:42 +0300

    add file ifTest.java

---
+++ MyFirstApp_ifTest.java
@@ -0,0 +1,12 @@
+public class MyFirstApp_ifTest {
+    public static void main (String[] args) {
+
+        int x = 2;
+        if (x == 3) {
+            System.out.println("x должен равняться 3");
+        } else {
+            System.out.println("x не равен 3");
+        }
+        System.out.println("Эта строка выполняется в любом случае");
+    }
+}
