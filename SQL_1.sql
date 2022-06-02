4609dc904c9c8d768726088f1dd9ea7dcab2f5bc
alexhills.la@gmail.com <aleks@Aleksandrs-MacBook-Pro.local>
2022-06-02 22:34:06 +0300

    sql

--- SQL_1.sql
+++ SQL_1.sql
@@ -1,7 +1,11 @@
-select * from students;
+select 'Gosha'; //выведется этот запрос и ничего более
 
-select * from students
+select * from students; // выбрать все из студентов
+
+select * from students 
 where name like 'James';
 
 select * from students
 where password like '12333';
+
+
